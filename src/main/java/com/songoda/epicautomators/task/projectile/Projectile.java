b58d30486d19241af412950f9537cc08876b247a
Brianna O'Keefe <bri@songoda.com>
2024-03-23 12:09:36 -0500

    Don't allow breaking other automators.

--- src/main/java/com/songoda/epicautomators/task/projectile/Projectile.java
+++ src/main/java/com/songoda/epicautomators/task/projectile/Projectile.java
@@ -12,9 +12,11 @@ import com.songoda.epicautomators.utils.ItemUtils;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Rotatable;
+import org.bukkit.block.data.type.Switch;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -85,6 +87,32 @@ public class Projectile {
             if (block.getState() instanceof Sign) {
                 // The projectile has hit a sign, calculate the bounce direction
                 direction = calculateBounceDirection(block);
+            } else if (block.getType().name().contains("BUTTON")) {
+                // The projectile has hit a button, press it
+                BlockData blockData = block.getBlockData();
+                if (blockData instanceof Switch) {
+                    Switch buttonSwitch = (Switch) blockData;
+                    buttonSwitch.setPowered(true);
+                    block.setBlockData(buttonSwitch);
+
+                    // Play a sound effect when the button is pressed
+                    XSound.BLOCK_STONE_BUTTON_CLICK_ON.play(location, 1.0F, 1.0F);
+
+                    // Create a particle effect at the button's location
+                    Location particleLocation = block.getLocation().add(0.5, 0.5, 0.5);
+                    location.getWorld().spawnParticle(Particle.CRIT, particleLocation, 10, 0.3, 0.3, 0.3, 0.1);
+
+                    // Schedule a task to deactivate the button after a delay
+                    Bukkit.getScheduler().runTaskLater(EpicAutomators.getInstance(), () -> {
+                        BlockState buttonState = block.getState();
+                        if (buttonState instanceof Switch) {
+                            Switch buttonSwitchState = (Switch) buttonState;
+                            buttonSwitchState.setPowered(false);
+                            buttonState.update(true);
+                        }
+                    }, 20L); // Adjust the delay as needed (20 ticks = 1 second)
+                    active = false;
+                }
             } else if (!block.getType().isAir() && isNotCrop) {
                 if (blocksDestroyed < automator.getMaxBlocks()) {
                     blocksDestroyed++;
